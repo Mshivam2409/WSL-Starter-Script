05015d7068c8495aabcc62b7744c2515be7aacdb
ShivamMalhotra1994 <shivammalhotra08@gmail.com>
2020-06-17 14:24:39 +0530

    Added Vscode extension Installer

--- vscode.bat
+++ vscode.bat
@@ -1,13 +1,13 @@
-code --install-extension CoenraadS.bracket-pair-colorizer-2
-code --install-extension eg2.vscode-npm-script
-code --install-extension EQuimper.react-native-react-redux
-code --install-extension esbenp.prettier-vscode
-code --install-extension ms-python.python
-code --install-extension ms-vscode-remote.remote-containers
-code --install-extension ms-vscode-remote.remote-wsl
-code --install-extension ms-vscode.cpptools
-code --install-extension mushan.vscode-paste-image
-code --install-extension PKief.material-icon-theme
-code --install-extension puorc.awesome-vhdl
-code --install-extension sdras.night-owl
-code --install-extension vscodevim.vim
+CALL code --install-extension CoenraadS.bracket-pair-colorizer-2
+CALL code --install-extension eg2.vscode-npm-script
+CALL code --install-extension EQuimper.react-native-react-redux
+CALL code --install-extension esbenp.prettier-vscode
+CALL code --install-extension ms-python.python
+CALL code --install-extension ms-vscode-remote.remote-containers
+CALL code --install-extension ms-vscode-remote.remote-wsl
+CALL code --install-extension ms-vscode.cpptools
+CALL code --install-extension mushan.vscode-paste-image
+CALL code --install-extension PKief.material-icon-theme
+CALL code --install-extension puorc.awesome-vhdl
+CALL code --install-extension sdras.night-owl
+CALL code --install-extension vscodevim.vim
