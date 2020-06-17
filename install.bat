bd756a084ff84b38afee3257e086c2b9925246ea
ShivamMalhotra1994 <shivammalhotra08@gmail.com>
2020-06-17 19:39:04 +0530

    Add Postman

--- install.bat
+++ install.bat
@@ -4,4 +4,6 @@ echo Installing Linux Packages
 CALL wsl bash -c "./wsl.sh"
 echo Installing Docker
 CALL winget install Docker.DockerDesktopEdge
-CALL wsl bash -c "./bash docker.sh"
+CALL wsl bash -c "./docker.sh"
+echo Installing Postman
+CALL winget install Postman.Postman
