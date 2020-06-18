cb12cdac5fa097261c9dd1db67b787f5f31b5ed4
Mshivam2409 <shivammalhotra08@gmail.com>
2020-06-18 15:21:25 +0530

    Update to WSL2

--- ubuntu.ps1
+++ ubuntu.ps1
@@ -1,5 +1,6 @@
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+wsl --set-default-version 2
 curl.exe -L -o ubuntu-2004.appx https://aka.ms/wsl-ubuntu-2004
 Rename-Item ubuntu-2004.appx ubuntu-2004.zip
 Expand-Archive ubuntu-2004.zip ubuntu
